297c0c5f7f8a1b420d8dcb760c3266e256064dbe
MelvDouc <melv.douc@gmail.com>
2021-01-25 01:58:24 +0100

    arriere-plans

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-let secs = setInterval(obtenirSecondes, 1000);
-let mins = setInterval(obtenirMinutes, 1000);
-let hours = setInterval(obtenirHeures, 1000);
+let temps = setInterval(obtenirTemps, 1000)
 
-function obtenirSecondes() {
+function obtenirTemps() {
     let secondes = document.querySelector('.cadre:nth-child(1)');
-    let d = new Date();
-    let s = d.getSeconds();
-    secondes.style.transform = `rotate(${s * 6}deg)`;
-}
-
-function obtenirMinutes() {
     let minutes = document.querySelector('.cadre:nth-child(2)');
-    let d = new Date();
-    let m = d.getMinutes();
-    minutes.style.transform = `rotate(${m * 6 + (d.getSeconds() / 60 * 6)}deg)`;
-}
-
-function obtenirHeures() {
     let heures = document.querySelector('.cadre:nth-child(3)');
     let d = new Date();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
     let h;
     if(d.getHours() >= 13){
         h = d.getHours() - 12
     } else{
         h = d.getHours();
     };
-    heures.style.transform = `rotate(${h * 30 + (d.getMinutes() / 60 * 30)}deg)`;
+    secondes.style.transform = `rotate(${s * 6}deg)`;
+    minutes.style.transform = `rotate(${(m + s / 60) * 6}deg)`;
+    heures.style.transform = `rotate(${(h + m / 60) * 30}deg)`;
 }
+
+//\\
+
+const horloge = document.querySelector('#horloge');
+const bgBtn = document.querySelector('#bg-btn');
+const arrierePlans = ['clock-arabic.gif', 'clock-arabic2.jpg', 'clock-arabic3.png', 'clock-no-numbers.png', 'clock-roman.png'];
+let index = 0;
+
+function changerImage() {
+    index = (++index % arrierePlans.length);
+    horloge.style.backgroundImage = `url('./img/${arrierePlans[index]}')`;
+}
+
+bgBtn.addEventListener('click', changerImage);
